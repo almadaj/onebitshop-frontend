f10c91cc77603499ac06064c02c251ebf5071633
Juliano Almada <almadaj@edu.unifor.br>
2024-02-02 13:57:04 -0300

    criação da NavBar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Container } from "./styles";
 
 import Header from "../../components/common/Header";
+import NavBar from "../../components/common/NavBar";
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <NavBar />
     </Container>
   );
 };
