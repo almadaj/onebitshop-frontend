4b4a4e6b184a3a13e66ee9cf78964ef01d540c2e
Juliano Almada <almadaj@edu.unifor.br>
2024-02-04 18:29:52 -0300

    criação do form de login e defaultbutton

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,14 +1,42 @@
 import React from "react";
-import { Container, Logo } from "./styles";
+import {
+  Container,
+  ForgetPassword,
+  Input,
+  InputContainer,
+  Logo,
+} from "./styles";
 import BackIcon from "../../components/common/BackIcon";
+import DefaultButton from "../../components/common/DefaultButton";
+import { Alert } from "react-native";
 
 const logo = require("../../../assets/images/logo.png");
 
 const Login = () => {
+  const handleLogin = () => {
+    Alert.alert("Botão de login clicado");
+  };
   return (
     <Container>
       <BackIcon marginLeft={30} />
       <Logo source={logo} />
+      <InputContainer>
+        <Input placeholder="Email" placeholderTextColor="white" />
+      </InputContainer>
+      <InputContainer>
+        <Input
+          placeholder="Senha"
+          placeholderTextColor="white"
+          secureTextEntry={true}
+        />
+      </InputContainer>
+      <ForgetPassword>Esqueceu sua senha?</ForgetPassword>
+      <DefaultButton
+        buttonText="Fazer Login"
+        buttonType="primary"
+        marginVertical={40}
+        buttonHandle={handleLogin}
+      />
     </Container>
   );
 };
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -14,3 +14,29 @@ export const Logo = styled.Image.attrs({ resizeMode: "contain" })`
   height: 200px;
   margin: 0 auto;
 `;
+
+export const InputContainer = styled.View`
+  width: 85%;
+  height: 50px;
+  border: 1px solid ${({ theme }) => theme.colors.borderColor};
+  background-color: ${({ theme }) => theme.colors.background};
+  border-radius: 5px;
+  margin: 0 auto;
+  margin-top: 20px;
+`;
+
+export const Input = styled.TextInput`
+  flex: 1;
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.primaryText};
+  padding-left: 10px;
+`;
+
+export const ForgetPassword = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  text-align: right;
+  margin-top: 10px;
+  margin-right: 9%;
+  color: ${({ theme }) => theme.colors.secondaryText};
+`;
