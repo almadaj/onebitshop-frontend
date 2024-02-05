33b00f42d70767da405b9f458a1f782936dc7ced
Juliano Almada <almadaj@edu.unifor.br>
2024-02-05 15:07:44 -0300

    estrutura de Search criada

---
+++ src/screens/Search/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "./styles";
+import Header from "../../components/common/Header";
+import NavBar from "../../components/common/NavBar";
+
+const Search = () => {
+  return (
+    <Container>
+      <Header />
+      <NavBar />
+    </Container>
+  );
+};
+
+export default Search;
