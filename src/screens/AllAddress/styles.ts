9297958675d57cd89aad6e8fa779e98bff37d6b1
Juliano Almada <almadaj@edu.unifor.br>
2024-02-15 13:41:26 -0300

    criação da tela de AllAddress

--- src/screens/AllAddress/styles.ts
+++ src/screens/AllAddress/styles.ts
@@ -8,3 +8,28 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.backgroundLight};
   padding-top: ${statusBarHeight + 20}px;
 `;
+
+export const NoAdd = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.primaryText};
+  text-align: center;
+  margin-top: 50%;
+  margin-bottom: 5%;
+`;
+
+export const CreateAddBtn = styled.TouchableOpacity`
+  width: 50%;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid ${({ theme }) => theme.colors.primaryButton};
+  margin: 0 auto;
+  border-radius: 15px;
+`;
+
+export const CreateAddBtnText = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.primaryText};
+`;
