dd9355944cbb93c9a3a639f3cbc6b08cbf5ad7cc
Juliano Almada <almadaj@edu.unifor.br>
2024-02-17 01:20:31 -0300

    estruturação de AddProduct

---
+++ src/screens/AddProduct/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Container } from "./styles";
+import DefaultTitle from "../../components/common/DefaultTitle";
+const AddProduct = () => {
+  return (
+    <Container>
+      <DefaultTitle title="CADASTRO DO ANÚNCIO" fontSize={18} />
+    </Container>
+  );
+};
+
+export default AddProduct;
