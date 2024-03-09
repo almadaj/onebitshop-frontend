089e32b824c36454493232f8417fa80b240da556
Juliano Almada <almadaj@edu.unifor.br>
2024-03-09 12:52:21 -0300

    criação de ChatList

--- src/screens/AllChats/index.tsx
+++ src/screens/AllChats/index.tsx
@@ -1,3 +1,4 @@
+import ChatList from "../../components/AllChats/ChatList";
 import DefaultTitle from "../../components/common/DefaultTitle";
 import NavBar from "../../components/common/NavBar";
 import { Container } from "./styles";
@@ -8,6 +9,7 @@ const AllChats = () => {
     <>
       <Container>
         <DefaultTitle title="CONVERSAS" fontSize={20} />
+        <ChatList />
       </Container>
       <NavBar />
     </>
