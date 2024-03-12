b29cf120c8c05c8f60caf90d227c5b3b986b7465
Juliano Almada <almadaj@edu.unifor.br>
2024-03-12 14:35:56 -0300

    criação do ChatHeader

--- src/screens/Chat/index.tsx
+++ src/screens/Chat/index.tsx
@@ -3,6 +3,7 @@ import { Container } from "./styles";
 import NavBar from "../../components/common/NavBar";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { PropsNavigationStack } from "../../routes";
+import ChatHeader from "../../components/Chat/ChatHeader";
 
 type Props = NativeStackScreenProps<PropsNavigationStack, "Chat">;
 
@@ -10,6 +11,10 @@ const Chat = ({ route }: Props) => {
   return (
     <>
       <Container>
+        <ChatHeader
+          sellerName={route.params.chatInfo.seller}
+          product={route.params.chatInfo.product}
+        />
         <NavBar />
       </Container>
     </>
